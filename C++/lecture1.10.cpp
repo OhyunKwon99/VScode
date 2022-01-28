6f0ed74d42fd3de781c7a9a323711f78d46ef8e9
OhyunKwon <96366791+OhyunKwon99@users.noreply.github.com>
2022-01-28 11:08:00 +0900

    220128 jason change

--- C++/lecture1.10.cpp
+++ C++/lecture1.10.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
+#include "lecture1.11.h"
 
 using namespace std;
 
-int add(int a, int b)
-{
-    return a + b;
-}
-
-int mult(int a, int b)
-{
-    return a + b;
-}
-
-int subt(int a, int b)
-{
-    return a + b;
-}
+///함수선언 declaration
+//int add(int a, int b); header로 이동
+int mult(int a, int b);
+int subt(int a, int b);
 
 int main()
 {
     cout << add(1, 2) << endl;
-    
+    return 0;
+}
+
+///함수정의 definition
+//int add(int a, int b)
+//{
+//    return a + b;
+//}
+/// header로 정의 이동
+int mult(int a, int b)
+{
+    return a * b;
+}
+
+int subt(int a, int b)
+{
+    return a - b;
 }
